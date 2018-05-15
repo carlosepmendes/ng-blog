17893fabb1f58e6f02c9acaf0a5d1a853565d1ed
carlos <carlosepmendes@gmail.com>
2018-05-15 21:41:05 +0000

    Added navbar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule
   ],
   exports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    NavbarComponent,
+    RouterModule
   ],
-  declarations: []
+  declarations: [NavbarComponent]
 })
 export class SharedModule { }
